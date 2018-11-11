b0539d66957a5085347ea04b4c2e1eef28dbfb54
phuongmyblog <44875907+phuongmyblog@users.noreply.github.com>
2018-11-11 21:15:30 +0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,60 @@
+<style>
+.qc-zone{/*border:1px solid #ebebeb;*/padding:15px;box-shadow:0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);transition-duration:200ms;transition-property: transform, box-shadow, margin, opacity, width;transition-timing-function:cubic-bezier(0.4, 0, 0.2, 1);border:none;transition:all .4s ease-in-out;}
+.qc-icon{width:50px;height:50px;position:absolute;float:right;z-index:15;right:-1px;background:url("https://4.bp.blogspot.com/-Lt5J5pYpCkQ/WC66lfw1qAI/AAAAAAAAEbg/EV3fJy_PwA4FbP06YvUALV-OcaIHq-rdgCLcB/s50/icon-hot.png") no-repeat scroll 0% 0% transparent;}
+.qc-text{position:relative;margin:0;padding:0;max-height:500px;overflow:hidden;max-width:320px;}
+.qc-text div{position:relative;background:#FFF;max-height:320px;padding:8px 0;margin:0;z-index:1;color:#555;border-top:1px solid #d8d8d8;}
+.qc-text h2.qc-header{position:relative;background:#FFF;font-size:14px;color:#f0523f;border-bottom:none;border-top:1px solid #ebebeb;border-radius:0;padding:8px 0;margin:6px 0;text-transform:none;cursor:pointer;font-weight:400;}
+.qc-text h2.qc-header:first-child{border-top:0} 
+.qc-text h2.qc-header:before{content:"";width:0;height:0;position:absolute;top:20px;right:15px;border:5px solid transparent;border-color:#b2b2b2 transparent transparent;cursor:pointer}
+.qc-content span.qc-title{font-size:18px;color:#f0523f;display:inline-block;cursor:pointer;}
+.qc-content span{font-weight:500;}
+.qc-content span.qc-url{font-size:12px;color:green;display:block;margin:8px 0;cursor:pointer;font-weight:400;}
+.qc-content span.qc-title:hover{text-decoration:underline}
+.qc-content span.qc-url:before{content:"\f05a";font-family:FontAwesome;margin:0 5px 0 0}
+.qc-content span.large-arrow{color:#fff;background:#f0523f;border-radius:4px;height:34px;float:right;width:34px;text-align:center;line-height:34px;cursor:pointer;transition:all .6s;position: absolute;bottom: 0;right:-36px;box-shadow:0 3px 5px rgba(0,0,0,0.1);}
+.qc-content span.large-arrow:before{content:"\f054";font-family:FontAwesome;margin:0 0 0 3px;font-size:20px}
+.qc-content span.large-arrow:hover{/*background:#292929;*/color:#fff;text-decoration:none;box-shadow:0 5px 10px rgba(0,0,0,0.2);}
+.qc-content p.content{padding:0;margin:0 40px 0 0;color:#555;font-size:12px;text-align:left;border-top:none;} 
+.qc-content p.content img{transition:background-color .3s ease 0s;transition:all .5s ease-in-out;transition-delay:.1s;}
+.qc-content p.content img:hover{-webkit-filter:grayscale(100%)}
+
+<div class='qc-icon'></div>
+<div class='qc-zone'>
+<div class='qc-text qc-text-sidebar' id='qc-text'>
+</style>
+    <!-- qc 1 -->
+    <div data-header="Thủ thuật Blogger">
+      <span class='qc-content'>
+<span class='qc-title' onclick="window.open('/search/label/Blogger%20Tips?&max-results=6');"><img src="https://2.bp.blogspot.com/-0Z1qP5egDnk/WDG15YhkBcI/AAAAAAAAEc0/zE7DZGC9HWEu-K8lhgJpGKWOGbOVS5pxQCLcB/s20/red-arrow.png" alt="Thủ thuật Blogger"/> Thủ thuật Blogger</span>
+      <span class='qc-url' onclick="window.open('/search/label/Blogger%20Tips?&max-results=6');">Chia sẻ những Thủ thuật Blogger mới nhất giúp tối ưu hóa trang Blogspot của bạn</span>
+      <p class='content'><a href="/search/label/Blogger%20Tips?&max-results=6" target="_blank"><img src="https://1.bp.blogspot.com/-u_bPPcQT5iQ/WCirQCRRW5I/AAAAAAAAEYY/mNS_FlRFvHU_W6q6X5rXm8ycVPNLujtCwCLcB/s320/Blogger-Tips-and-Tricks.png" alt="Thủ thuật Blogger" title="Thủ thuật Blogger"/></a><span class='large-arrow' onclick="window.open('/search/label/Blogger%20Tips?&max-results=6');"></span></p>
+      </span>
+    </div>
+	 <!-- qc 2 -->
+    <div data-header="Trợ giúp Blogger">
+      <span class='qc-content'>
+<span class='qc-title' onclick="window.open('/p/premium-services.html');"><img src="https://2.bp.blogspot.com/-0Z1qP5egDnk/WDG15YhkBcI/AAAAAAAAEc0/zE7DZGC9HWEu-K8lhgJpGKWOGbOVS5pxQCLcB/s20/red-arrow.png" alt="Trợ giúp Blogger"/> Trợ giúp Blogger</span>
+      <span class='qc-url' onclick="window.open('/p/premium-services.html');">Bạn gặp khó khăn trong việc sử dụng Blogger? Hãy để chúng tôi giúp bạn</span>
+      <p class='content'><a href="/p/premium-services.html" target="_blank"><img src="https://2.bp.blogspot.com/-ZSwxFiL9-_A/WMMBdZNTUvI/AAAAAAAAFKg/z2VLvtQuopsP6DoaeJmykl7WYjA9VgLtgCLcB/s320/blogger-help.jpg" alt="Trợ giúp Blogger" title="Trợ giúp Blogger"/></a><span class='large-arrow' onclick="window.open('/p/premium-services.html');"></span></p>
+      </span>
+    </div>
+
+    <!-- qc 3 -->
+    <div data-header="Gửi bài viết">
+      <span class='qc-content'>
+<span class='qc-title' onclick="window.open('/p/send-post.html');"><img src="https://2.bp.blogspot.com/-0Z1qP5egDnk/WDG15YhkBcI/AAAAAAAAEc0/zE7DZGC9HWEu-K8lhgJpGKWOGbOVS5pxQCLcB/s20/red-arrow.png" alt="Free Backlinks"/> Gửi bài viết</span>
+      <span class='qc-url' onclick="window.open('/p/send-post.html');">Viết Guest Post và nhận Free Backlinks Top Google</span>
+      <p class='content'><a href="/p/send-post.html" target="_blank"><img src="https://4.bp.blogspot.com/-xX7bBfYZiK8/WMtql_TeDvI/AAAAAAAAFMQ/wD0_oR42mHgSfLqSgKBv9UQ0MpFZA6HvACLcB/s250/free-backlinks.png" alt="Free Backlinks" title="Free Backlinks"/></a><span class='large-arrow' onclick="window.open('/p/send-post.html');"></span></p>
+      </span>
+    </div>
+
+<!-- qc 4 
+    <div data-header="Biểu tượng cảm xúc Facebook">
+      <span class='qc-content'>
+<span class='qc-title' onclick="window.open('/p/icon-facebook.html');"><img src="https://2.bp.blogspot.com/-0Z1qP5egDnk/WDG15YhkBcI/AAAAAAAAEc0/zE7DZGC9HWEu-K8lhgJpGKWOGbOVS5pxQCLcB/s1600/red-arrow.png" alt="Mô hình giấy"/> Biểu tượng cảm xúc Facebook</span>
+      <span class='qc-url' onclick="window.open('/p/icon-facebook.html');">Trọn bộ Icon Facebook đầy ngộ nghĩnh, đáng yêu</span>
+      <p class='content'><a href="/p/icon-facebook.html" target="_blank"><img src="https://2.bp.blogspot.com/-gJLI5V41hlE/WMMBdmiXN3I/AAAAAAAAFKk/W8kG4djQdUo7pMfDH-nR1leBwBORof2TACLcB/s320/facebook-emojis-icon.png" alt="Biểu tượng cảm xúc Facebook" title="Biểu tượng cảm xúc Facebook"/></a><span class='large-arrow' onclick="window.open('/p/icon-facebook.html');"></span></p>
+      </span>
+    </div> -->
+
+  </div></div>
